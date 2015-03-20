9013b7f4170923e20b040a9725c71a4354e09535
John Salaveria <jsalaveria@klick.com>
2015-03-20 16:46:36 -0400

    redesign: first build of likes counter, updated work modules

---
+++ ig-ajax.js
@@ -0,0 +1,39 @@
+const CLIENT_ID = "client_id=7812748ec4914c59ad0dcfbfe1130992";
+const INSTAGRAM_AUTH = "https://instagram.com/oauth/authorize/?";
+const REDIRECT_URI = "http://johnsington.me/ig-ajax.html";
+
+function json(url, data, callback) {
+    $.ajax({
+        url: url,
+        dataType: "json",
+        data: data || null
+    }).done(function(res) {
+        callback(res);
+    });
+}
+
+function authenticateUser(){
+	var authenticate_url = INSTAGRAM_AUTH + CLIENT_ID + "&redirect_uri=" + REDIRECT_URI + "&response_type=token";
+	console.log(authenticate_url);
+	window.location.href = authenticate_url;
+}
+
+function getAccessToken(){
+	if (location.hash)
+	{
+		var access_token = location.hash.substring(14);
+	}
+	else
+	{
+		console.log(location.hash);
+	}
+
+}
+console.log(location);
+window.onhashchange = getAccessToken();
+
+$(document).ready(function(){
+	$("a").click(function(){
+		authenticateUser();
+	});
+});
