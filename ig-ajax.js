283da9cf5d1c361286d764bb58df0dc89f9e08ba
johnsington <jj.salaveria@gmail.com>
2015-03-26 22:37:07 -0400

    ig likes: testing basic user flow

--- ig-ajax.js
+++ ig-ajax.js
@@ -2,11 +2,14 @@ const CLIENT_ID = "client_id=7812748ec4914c59ad0dcfbfe1130992";
 const INSTAGRAM_AUTH = "https://instagram.com/oauth/authorize/?";
 const REDIRECT_URI = "http://johnsington.me/ig-ajax.html";
 
-function json(url, data, callback) {
+//for offline testing, @johnsington access token
+const testUser = "18354893.7812748.bfd6af94ff914bb28374abd75e64a11d";
+
+function json(url, callback) {
     $.ajax({
         url: url,
-        dataType: "json",
-        data: data || null
+        dataType: "jsonp",
+        data: { format:"json"}
     }).done(function(res) {
         callback(res);
     });
@@ -18,12 +21,12 @@ function authenticateUser(){
 	window.location.href = authenticate_url;
 }
 
-function getAccessToken(){
+function main(){
+	var access_token = "";
+	//gets access token from URL after hash changes
 	if (location.hash)
 	{
-		var access_token = location.hash.substring(14);
-		console.log(access_token);
-		return access_token;
+		access_token = location.hash.substring(14);
 	}
 	else
 	{
@@ -31,11 +34,41 @@ function getAccessToken(){
 		return false;
 	}
 
+	getUserPhotos(access_token, 
+		function(data){
+			console.log('here');
+			var likes = countLikes(data);
+			$('#resp').text(likes + " likes");
+		});
+
 }
 
+//returns Object of photos
+function getUserPhotos(token, callback){
+	// token = testUser;
+	console.log('entered getUserPhotos');
+	json("https://api.instagram.com/v1/users/self/media/recent/?access_token="
+			+ token,
+		function(data){
+			console.log(data);
+			callback(data);
+		}
+		)
+}
+
+function countLikes(data){
+	var likeCount = 0;
+	$.each(data.data, function(index, val){
+		var cur_like = val.likes.count;
+		console.log("like from " + index + ": " + cur_like);
+		likeCount += cur_like;
+		console.log("total from 20 posts: " + likeCount);
+	});
+	return likeCount;
+}
 
 console.log(location);
-window.onhashchange = getAccessToken();
+window.onhashchange = main();
 
 $(document).ready(function(){
 	$("a").click(function(){
