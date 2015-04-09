3f49c3eba4dc247ff9cb9dd2e99d7cf4f2fd24ae
johnsington <jj.salaveria@gmail.com>
2015-04-08 23:56:14 -0400

    redesign: integrated work modules

--- site.js
+++ site.js
@@ -1,4 +1,43 @@
+//Card modules
+function cardInit(id){
+	$(id).on({
+		mouseover: function(){
+			$(this).addClass("hover");
+		},
+		mouseleave: function(){
+			if(!$(this).hasClass("shiftup")){
+				$(this).removeClass("hover");
+			}
+		},
+		click: function(){
+			//opens clicked card
+			if(!$(this).hasClass("shiftup")){
+				//resets all active cards
+				$(".slider").removeClass("shiftup");
+				$(".desc").removeClass("shiftup");
+				//opens active card
+				$(this).addClass("shiftup");
+				$(".desc", this).addClass("shiftup").css("height", $(".card").css("height"));
+			}
+			else{
+				$(this).removeClass("shiftup");
+				$(".desc", this).removeClass("shiftup");
+			}
+		}
+	});
+}
+
+function resizeLanding(){
+	if($(window).height()>600){
+	$("header, .overlay").css("height", $(window).height());
+	}
+	else{
+		$("header, .overlay").css("height", "600px");
+	}
+}
+
 $(document).ready(function(){
+	resizeLanding();
 
 	//initial animations
 	$(window).load(function(){
@@ -16,11 +55,13 @@ $(document).ready(function(){
 	}, 500);
 	});
 	
+	//initialize card funcationality
+	// $(".slider").each(function(){
+	// 	cardInit("#" + $(this).attr("id"));
+	// });
 
-	if($(window).height()>600){
-	$("header").css("height", $(window).height());
-	}
-	else{
-		$("header").css("height", "600px");
-	}
+	//resizes landing to fit web client height
+	$(window).resize(function(){
+		resizeLanding();
+	})
 })
